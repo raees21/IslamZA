82fb27934e3c7fbbd8cc2dbe56e568ef82ef4f19
Raees <raeesaziz786@gmail.com>
2021-06-27 19:44:06 +0200

    Initial commit

---
+++ IslamZA/IslamZA/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using IslamZA.ViewModels;
+using IslamZA.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace IslamZA
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
