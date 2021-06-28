99b536c9da34d7cdc094edf9253e86c196697a90
Raees <raeesaziz786@gmail.com>
2021-06-28 09:25:12 +0200

    Initial Settings
    Initial Settings

--- IslamZA/IslamZA/ViewModels/AboutViewModel.cs
+++ IslamZA/IslamZA/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace IslamZA.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "IslamZA";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
